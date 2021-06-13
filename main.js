b9de38f647626cd46236893533176b6ddb6afde6
Dragon Master <73437981+ReynaldoSoto@users.noreply.github.com>
2021-06-13 11:03:32 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 status = "";
-object = "";
+objects = [];
 
 function setup(){
     canvas = createCanvas(630, 490);
@@ -11,7 +11,7 @@ function setup(){
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
-    object = document.getElementById("input_name").value;
+    object_name = document.getElementById("input_name").value;
 }
 
 function modelLoaded(){
@@ -19,6 +19,38 @@ function modelLoaded(){
     status = true;
 }
 
+function gotResult(error, results){
+    if(error){
+     console.log(error);
+    }
+   console.log(results); 
+   objects = results;
+}
+
 function draw(){
     image(video, 0, 0, 630, 490);
+
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+         for(i = 0; i < objects.length; i++){
+             document.getElementById("status").innerHTML = "Status: Object Detected";
+             fill("#FF0000");
+             percent = floor(objects[i].confidence * 100);
+             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 20);
+             noFill();
+             stroke("#FF0000");
+             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+             if(objects[i].label == object_name){
+                 video.stop();
+                 objectDetector.detect(gotResult);
+                 document.getElementById("status_on_mentioned_object").innerHTML = object_name + " found";
+                 synth = window.speechSynthesis;
+                 utterThis = new SpeechSynthesisUtterance(object_name + "Found");
+                 synth.speak(utterThis);
+             } else{
+                 document.getElementById("status_on_mentioned_object").innerHTML = object_name + " not found";
+             }
+         }
+    }
 }
